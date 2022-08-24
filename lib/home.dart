0e2a8815b7a3d2d75ade7bcb339934d582b2bcf2
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2022-08-24 19:56:11 +0800

    Did up a bit of profile view

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scouts_flutter/main.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -52,13 +53,125 @@ class _HomeState extends State<Home> {
                     style: TextStyle(
                       color: Theme.of(context).colorScheme.primary,
                     ),
-                  
                   ),
                 ),
                 const Spacer(),
               ],
             ),
           ),
+          // Menu Bar Ends Here
+
+          Container(
+            padding: const EdgeInsets.only(top: 50),
+            child: Column(
+              children: [
+                Text(
+                  userPerson!.name,
+                  style: Theme.of(context)
+                      .textTheme
+                      .displayLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.primary),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  "SST Fearless Falcons Unit, Patrol ${userPerson!.patrol}",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineSmall!
+                      .copyWith(color: Theme.of(context).colorScheme.primary),
+                ),
+                const SizedBox(height: 50),
+
+                // Achievements
+                Text(
+                  "Achievements",
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.primary),
+                ),
+                const SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Spacer(),
+                    Container(
+                      height: 275,
+                      width: 450,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        children: const [
+                          Spacer(),
+                          Text(
+                            "Add Image here",
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Spacer(),
+                          Text(
+                            "Filler 1",
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                      height: 275,
+                      width: 450,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        children: const [
+                          Spacer(),
+                          Text(
+                            "Add Image here",
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Spacer(),
+                          Text(
+                            "Filler 1",
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    Container(
+                      height: 275,
+                      width: 450,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        children: const [
+                          Spacer(),
+                          Text(
+                            "Add Image here",
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Spacer(),
+                          Text(
+                            "Filler 1",
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
