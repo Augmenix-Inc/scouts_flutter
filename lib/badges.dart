1f86622f59d95f3e0b68fa857ece3963e661f5c0
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2022-08-27 16:24:20 +0800

    Made badges.dart and added menu bar in it

---
+++ lib/badges.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:scouts_flutter/main.dart';
+
+class Badges extends StatefulWidget {
+  const Badges({Key? key}) : super(key: key);
+
+  @override
+  State<Badges> createState() => _BadgesState();
+}
+
+class _BadgesState extends State<Badges> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          // Menu Bar -> Shift to a widgets so that it can be reused
+          Container(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            height: 50,
+            padding: const EdgeInsets.only(left: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo.jpg',
+                  fit: BoxFit.cover,
+                  height: 40,
+                ),
+                const Spacer(),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "UNIT",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "BADGES",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    "MY SCOUTING JOURNEY",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+          // Menu Bar Ends Here
+        ],
+      ),
+    );
+  }
+}
