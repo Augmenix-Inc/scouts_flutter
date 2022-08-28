365775cd73bd1cb9fa644c4acb31ba948e453b82
DakshRocks21 <85815311+DakshRocks21@users.noreply.github.com>
2022-08-28 22:43:13 +0800

    Added achievements items

--- lib/home.dart
+++ lib/home.dart
@@ -66,118 +66,88 @@ class _HomeState extends State<Home> {
             ),
           ),
           // Menu Bar Ends Here
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(top: 50),
+              child: Column(
+                children: [
+                  Text(
+                    userPerson!.name,
+                    style: Theme.of(context)
+                        .textTheme
+                        .displayLarge!
+                        .copyWith(color: Theme.of(context).colorScheme.primary),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "SST Fearless Falcons Unit, Patrol ${userPerson!.patrol}",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headlineSmall!
+                        .copyWith(color: Theme.of(context).colorScheme.primary),
+                  ),
+                  const SizedBox(height: 50),
 
-          Container(
-            padding: const EdgeInsets.only(top: 50),
-            child: Column(
-              children: [
-                Text(
-                  userPerson!.name,
-                  style: Theme.of(context)
-                      .textTheme
-                      .displayLarge!
-                      .copyWith(color: Theme.of(context).colorScheme.primary),
-                ),
-                const SizedBox(height: 10),
-                Text(
-                  "SST Fearless Falcons Unit, Patrol ${userPerson!.patrol}",
-                  style: Theme.of(context)
-                      .textTheme
-                      .headlineSmall!
-                      .copyWith(color: Theme.of(context).colorScheme.primary),
-                ),
-                const SizedBox(height: 50),
-
-                // Achievements
-                Text(
-                  "Achievements",
-                  style: Theme.of(context)
-                      .textTheme
-                      .titleLarge!
-                      .copyWith(color: Theme.of(context).colorScheme.primary),
-                ),
-                const SizedBox(height: 25),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Spacer(),
-                    Container(
-                      height: 275,
-                      width: 450,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        children: const [
-                          Spacer(),
-                          Text(
-                            "Add Image here",
-                            overflow: TextOverflow.ellipsis,
+                  // Achievements
+                  Text(
+                    "Achievements",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(color: Theme.of(context).colorScheme.primary),
+                  ),
+                  const SizedBox(height: 25),
+                  Expanded(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: GridView.builder(
+                            itemCount: 3,
+                            gridDelegate:
+                                const SliverGridDelegateWithMaxCrossAxisExtent(
+                              crossAxisSpacing: 20,
+                              mainAxisSpacing: 20,
+                              mainAxisExtent: 150,
+                              maxCrossAxisExtent: 500,
+                            ),
+                            itemBuilder: (context, index) {
+                              final achievements = userPerson!.progress;
+                              return Container(
+                                padding: const EdgeInsets.all(10),
+                                child: Card(
+                                  child: Row(children: [
+                                    const SizedBox(width: 20),
+                                    Padding(
+                                      padding: const EdgeInsets.all(10),
+                                      child: Image.asset(
+                                          'assets/patrol/zetta.gif'),
+                                    ),
+                                    const SizedBox(width: 20),
+                                    Column(
+                                      children: [
+                                        const SizedBox(height: 20),
+                                        Text(achievements[index].badgeName),
+                                        const SizedBox(height: 20),
+                                        Text(
+                                            achievements[index].dateCompletion),
+                                        const SizedBox(height: 20),
+                                      ],
+                                    ),
+                                    const SizedBox(width: 20),
+                                  ]),
+                                ),
+                              );
+                            },
                           ),
-                          Spacer(),
-                          Text(
-                            "Filler 1",
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Spacer(),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    const Spacer(),
-                    Container(
-                      height: 275,
-                      width: 450,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        children: const [
-                          Spacer(),
-                          Text(
-                            "Add Image here",
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Spacer(),
-                          Text(
-                            "Filler 1",
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                    ),
-                    const Spacer(),
-                    Container(
-                      height: 275,
-                      width: 450,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Row(
-                        children: const [
-                          Spacer(),
-                          Text(
-                            "Add Image here",
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Spacer(),
-                          Text(
-                            "Filler 1",
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          Spacer(),
-                        ],
-                      ),
-                    ),
-                    const Spacer(),
-                  ],
-                )
-              ],
+                  ),
+                ],
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
